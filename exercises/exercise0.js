456ca064dfe04e1f1ebb42daafdb76716e9e7476
JanetS <s_yiting@cda080ev5815x.local>
2019-09-12 16:10:45 -0400

    Adding exercises& templates

---
+++ exercises/exercise0.js
@@ -0,0 +1,109 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+  createCanvas(500,500);
+  background(255,255,102);
+  noStroke();
+  rectMode(CENTER);
+  //GREEN
+  fill(0,255,20);
+  rect(250,250,450,450);
+  //PINK
+  fill(255,51,204);
+  rect(250,250,400,400);
+  //blue
+  fill(0, 204, 255);
+  rect(250,250,380,380);
+  //yellow
+  fill(255, 255, 0);
+  rect(250,250,350,350);
+  //green
+  fill(0,255,20);
+  rect(250,250,300,300);
+  //PINK
+  fill(255,51,204);
+  rect(250,250,270,270);
+  //blue
+  fill(0, 204, 255);
+  rect(250,250,250,250);
+  //yellow
+  fill(255, 255, 0);
+  rect(250,250,220,220);
+  //green
+  fill(0,255,20);
+  rect(250,250,190,190);
+  //PINK
+  fill(255,51,204);
+  rect(250,250,160,160);
+  //blue
+  fill(0, 204, 255);
+  rect(250,250,150,150);
+  //yellow
+  fill(255, 255, 0);
+  rect(250,250,135,135);
+  //green
+  fill(0,255,20);
+  rect(250,250,110,110);
+  //PINK
+  fill(255,51,204);
+  rect(250,250,100,100);
+  //blue
+  fill(0, 204, 255);
+  rect(250,250,90,90);
+  //yellow
+  fill(255, 255, 0);
+  rect(250,250,70,70);
+  filter(BLUR,8);
+
+
+  //EYE White
+  fill(242);
+  ellipse(250,360,250,250);
+  fill(255, 204, 204,30);
+  ellipse(250,360,250,250);
+  fill(250);
+  ellipse(270,330,180,180);
+  fill(255);
+  ellipse(280,320,150,150);
+  //pupil
+  fill(26,13,0);
+  ellipse(300,310,100,100);
+  fill(77,38,0,130 );
+  ellipse(305,305,80,80);
+  fill(255,120);
+  ellipse(315,305,40,40);
+  fill(255,200);
+  ellipse(315,325,10,10);
+  fill(255,200);
+  ellipse(325,290,15,15);
+
+
+
+
+
+
+
+}
+function draw() {
+}
