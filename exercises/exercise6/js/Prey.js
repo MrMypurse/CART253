e4e73cf4a5287d2635a24302cb8be0b34cf8d17f
Janet <sun19980@gmail.com>
2019-10-24 16:59:39 -0400

    Ex6: debug Prey
    change (this.x > 0 ) to (this.x < 0 ) in handleWrapping function

--- exercises/exercise6/js/Prey.js
+++ exercises/exercise6/js/Prey.js
@@ -59,7 +59,8 @@ class Prey {
     // wraps it to the other side if so
     handleWrapping() {
       // Off the left or right
-      if (this.x > 0) {
+      //////////////// FIXED
+      if (this.x < 0) {
         this.x += width;
       }
       else if (this.x > width) {
