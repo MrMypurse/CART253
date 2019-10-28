9e7b854a7badba7507893d178d8db91b75c601cb
Janet Sun <sun19980@gmail.com>
2019-10-28 16:00:54 -0400

    Ex6: fix Predator.js
    fix typo: change "pish()" to "push()"

--- exercises/exercise6/js/Predator.js
+++ exercises/exercise6/js/Predator.js
@@ -134,7 +134,8 @@ class Predator {
   // Draw the predator as an ellipse on the canvas
   // with a radius the same size as its current health.
   display() {
-    pish();
+    //////////////// FIXED
+    push();
     noStroke();
     fill(this.fillColor);
     this.radius = this.health;
