cf5e90b3fdb81b8efe707f46f0d2497fda7ec48c
Janet Sun <sun19980@gmail.com>
2019-10-28 16:17:34 -0400

    Ex6: fix script.js
    fix typo: change"backgroun" to "background"

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -40,7 +40,8 @@ function setup() {
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
   // Clear the background to black
-  backgroun(0);
+  //////////////// FIXED
+  background(0);
 
   // Handle input for the tiger
 
