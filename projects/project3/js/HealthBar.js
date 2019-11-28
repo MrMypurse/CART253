d43864aee964f2717fe7ba72cacfa8f3db6c6561
Janet Sun <sun19980@gmail.com>
2019-11-28 15:19:29 -0500

    ex8: adjust images
    1.adjust images values so they look better
    2. duplicate ex8 as project3

---
+++ projects/project3/js/HealthBar.js
@@ -0,0 +1,40 @@
+// HealthBar
+//
+// A class that represents the player's health
+// the width of bar is relative to the health
+
+
+class HealthBar {
+  // constructor
+  //
+  // Sets the initial values for the ScoreBar's properties
+  // Either sets default values or uses the arguments provided
+  constructor(x, y, fillColor, height) {
+    // Position
+    this.x = x;
+    this.y = y;
+    // Score properties
+    // Display properties
+    this.fillColor = fillColor;
+    this.width = player.health;
+    this.height = height;
+  }
+
+  // display
+  //
+  // Draw the health bar as a rectangle on the canvas
+  // with a width the same size as its current health.
+
+  display() {
+    push();
+    noStroke();
+    fill(this.fillColor);
+    rect(this.x, this.y, -this.width, this.height);
+    pop();
+  }
+
+    // width is defined in terms of the player's health
+  updateHealth(health) {
+    this.width = health * 2.5;
+  }
+}
---
+++ projects/project3/js/Player.js
@@ -0,0 +1,140 @@
+// player
+//
+// A class that represents a simple player
+// controlled by the arrow keys. It can move around
+// the screen and consume supply objects to maintain its health.
+
+class Player {
+
+  // constructor
+  //
+  // Sets the initial values for the player's properties
+  // Either sets default values or uses the arguments provided
+  constructor(x, y, speed, fillColor, radius) {
+    // Position
+    this.x = x;
+    this.y = y;
+    // Velocity and speed
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = speed;
+    // Health properties
+    this.maxHealth = radius;
+    this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
+    this.healthLossPerMove = 0.07;
+    this.healthGainPerEat = 1;
+    // Display properties
+    this.fillColor = fillColor;
+    this.radius = this.health; // Radius is defined in terms of health
+    // Input properties
+    this.leftKey = 65;
+    this.rightKey = 68;
+  }
+
+  // handleInput
+  //
+  // Checks if an arrow key is pressed and sets the player's
+  // velocity appropriately.
+  handleInput() {
+    // Horizontal movement
+    if (keyIsDown(this.leftKey)) {
+      this.vx = -this.speed;
+    } else if (keyIsDown(this.rightKey)) {
+      this.vx = this.speed;
+    } else {
+      this.vx = 0;
+    }
+  }
+
+  // move
+  //
+  // Updates the position according to velocity
+  // Lowers health (as a cost of living)
+  // Handles wrapping
+  move() {
+    // Update position
+    this.x += this.vx;
+    this.y += this.vy;
+    // Update health
+    this.health = this.health - this.healthLossPerMove;
+    this.health = constrain(this.health, 0, this.maxHealth);
+    // Handle wrapping
+    this.handleWrapping();
+  }
+
+  // handleWrapping
+  //
+  // Checks if the player has gone off the canvas and
+  // wraps it to the other side if so
+  handleWrapping() {
+    // Off the left or right
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+    // Off the top or bottom
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
+  // handleEating
+  //
+  // Takes a supply object as an argument and checks if the player
+  // overlaps it. If so, reduces the supply's health and increases
+  // the player's. If the supply dies, it gets reset.
+  handleEating(supply) {
+    // Calculate distance from this player to the supply and the waste
+    let d = dist(this.x, this.y, supply.x, supply.y);
+    // Check if the distance is less than their two radius (an overlap)
+    if (d < this.radius + supply.radius) {
+      if (supply instanceof Waste === false){
+        // Increase player health when touching supply
+        this.health += this.healthGainPerEat;
+      }else {
+        // Decrease player health when touching wastes
+        this.health -= this.healthGainPerEat * 6;
+      }
+
+      this.health = constrain(this.health, 0, this.maxHealth);
+      // Decrease supply health by the same amount
+      supply.health -= this.healthGainPerEat * 5;
+      // Check if the supply died and reset it if so
+      if (supply.health < 2) {
+        supply.reset();
+      }
+    }
+  }
+
+
+  // display
+  //
+  // Draw the player as an ellipse on the canvas
+  // with a radius the same size as its current health.
+  display() {
+    push();
+    noStroke();
+    fill(this.fillColor);
+    this.radius = this.health;
+    if (this.radius > 2) {
+      ellipse(this.x, this.y, this.radius * 2);
+    }
+    pop();
+  }
+
+  //endGame
+  //
+  //End the main game when player'health is below 0
+  endGame() {
+    if (this.health <= 0) {
+      state = "GAMEOVER";
+      return;
+    //} else if (this.score >= 0) {
+    //  state = "GAMEWIN";
+    //  return;
+    }
+  }
+}
