870164bf0d39aa4efa8af36012b5669eb50bb63f
Janet Sun <sun19980@gmail.com>
2019-11-28 15:42:12 -0500

    pj3: add flashlight class
    1. add flashlight class and properties

---
+++ projects/project3/js/Flashlight.js
@@ -0,0 +1,19 @@
+// Flashlight
+//
+// A class that represents the flashlight function
+// flashlight is turned on/off by clicking the mouse
+class Flashlight {
+  // constructor
+  //
+  // Sets the initial values for the flashlight's properties
+  // Either sets default values or uses the arguments provided
+  constructor(x, y){
+    // Position
+    this.x = x;
+    this.y = y;
+    //
+    let lightRadius = 90;
+    //pointLight()
+    //create blakc image with transparency in the middle.
+  }
+}
