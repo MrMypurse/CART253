b528a99ab19a1c7610ea47f8b72b789bd81d55e4
Janet Sun <sun19980@gmail.com>
2019-12-03 10:27:13 -0500

    pj3: add flahslight class
    1. add flashlight image
    2, add toggleOn function to turn on/ off the flashlight

--- projects/project3/js/Flashlight.js
+++ projects/project3/js/Flashlight.js
@@ -7,13 +7,23 @@ class Flashlight {
   //
   // Sets the initial values for the flashlight's properties
   // Either sets default values or uses the arguments provided
-  constructor(x, y){
+  constructor(x, y, flashOn, flashImage){
     // Position
     this.x = x;
     this.y = y;
-    //
-    let lightRadius = 90;
-    //pointLight()
+    //Display Properties
+    this.flashOn = flashOn;
+    imageMode(CENTER);
+    rectMode(CENTER);
+    this.image = flashImage;
     //create blakc image with transparency in the middle.
+  }
+
+  toggleFlash(){
+    if (flashOn === true){
+      image(flashImage, this.x, this.y, this.width * 2, this.height * 2);
+    } else{
+      rect(window.width/2, window.height/2, window.width, window.height);
+    }
   }
 }
